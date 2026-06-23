a473bc30328a2679241b3ce5a9a440101326c360
SheepYhangCN <sheepyuhang@163.com>
2026-06-23 22:54:35 +0800

    optimize description of Backup files detected

--- Main.cs
+++ Main.cs
@@ -488,7 +488,7 @@ public partial class Main : Control
 				nodeWindowPatchContent.Text = "locBakDetected";
 			}
 			nodeWindowPatchContent.Set("theme_override_font_sizes/font_size", FontSize(27, windowScale));
-			nodeWindowPatch.Size = new Vector2I(640, 320) * windowScale;
+			nodeWindowPatch.Size = new Vector2I(TranslationServer.GetLocale().StartsWith("en") ? 800 : 640, 320) * windowScale;
 			nodeWindowPatchVBox.CustomMinimumSize = new Vector2(640, 0) * windowScale;
 			nodeWindowPatch.Show();
 		}
