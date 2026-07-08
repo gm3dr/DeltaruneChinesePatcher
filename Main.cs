1f4bb372958bf9602c39ee989fb283a9e64391f3
SheepYhangCN <sheepyuhang@163.com>
2026-07-08 13:23:14 +0800

    update xdelta3_mac to 3.2.0

--- Main.cs
+++ Main.cs
@@ -103,7 +103,7 @@ public partial class Main : Control
 		{GetGameDirPath("externals/7zip/7z_mac"), "113122403b762d2f838aff4e68cbd37066cdb7e585e655ff1155f846287c5b8a"},
 		{GetGameDirPath("externals/xdelta3/xdelta3"), "0d38d86de5ab6bbc1adae531331d64585b5a09ce3604a5f090c27f71b6a64b23"},
 		{GetGameDirPath("externals/xdelta3/xdelta3.exe"), "53d90226615f217d3380c39892833311b4e24acd863e1ca01f14b5e772e2e6d0"},
-		{GetGameDirPath("externals/xdelta3/xdelta3_mac"), "a425bc68caec30b075d4f4c94f4042ad421c6477ed4903f4594a32446cd59b79"}
+		{GetGameDirPath("externals/xdelta3/xdelta3_mac"), "3e9e590657979fc83b43d6e549d929dc1e5ddb01b0a85509431423c036720e83"}
 	};
 	static readonly Godot.Collections.Dictionary<string, Godot.Collections.Dictionary<string, string>> default_paths = new()
 	{
